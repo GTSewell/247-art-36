e3f09cb7c6bd816414a15a360eef0292b6598777
GTSewell <115980461+GTSewell@users.noreply.github.com>
2025-03-17 13:53:13 +1100

    Update FeatureRow.tsx

--- src/components/details/FeatureRow.tsx
+++ src/components/details/FeatureRow.tsx
@@ -20,7 +20,7 @@ const FeatureRow = ({ feature, index, isExpanded, toggleRow }: FeatureRowProps) 
         className={cn(
           index % 2 === 0 ? "bg-white" : "bg-gray-50",
           isExpanded ? "border-b-0" : "",
-          "cursor-pointer hover:bg-zap-white"
+          "cursor-pointer hover:bg-zap-blue"
         )} 
         style={{ transition: 'none' }}
         onClick={() => toggleRow(index)}
