6371b364635addec2e5ac7f7e990b3e74e6aff66
GTSewell <115980461+GTSewell@users.noreply.github.com>
2025-03-23 16:32:56 +1100

    Update JoinUndergroundSection.tsx

--- src/components/sections/JoinUndergroundSection.tsx
+++ src/components/sections/JoinUndergroundSection.tsx
@@ -28,10 +28,10 @@ const JoinUndergroundSection = () => {
           duration: 0.5,
           delay: 0.2
         }} className="mb-8 text-center">
-            <p className="text-xl md:text-2xl font-extrabold text-white">
-              <span className="block">We built Lanes End</span>
-              <span className="block">We built VS. Gallery</span>
-              <span className="block">We built Milkbar</span>
+            <p className="text-xl md:text-3xl font-extrabold text-white">
+              <span className="block">We built 'Lanes End'</span>
+              <span className="block">We built 'VS. Gallery'</span>
+              <span className="block">We built 'Milkbar'</span>
               <span className="block mt-4">Now, we build 247!</span>
             </p>
           </motion.div>
